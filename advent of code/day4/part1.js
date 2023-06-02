682623969099b166e5b73681a6821311833f971e
sumedhrao7 <sumedhrao7@gmail.com>
2023-06-02 08:23:01 +0530

    day4 using sets

--- advent of code/day4/part1.js
+++ advent of code/day4/part1.js
@@ -3,16 +3,24 @@ const { data } = require('./input')
 
 counter = 0;
 
+const getRange = (size, startAt = 0) => {
+	return [...Array(size).keys()].map( i => i + startAt);
+}
+
+const eqSetCheck = (set1, set2) => 
+	[...set1].every((x) => 	set2.has(x));
+
 data.forEach((ele) => {
 	const [ section1, section2 ] = [...ele.split(",")]
 	const [ section1First, section1Second ] = [...section1.split("-")].map( ele => parseInt(ele))
 	const [ section2First, section2Second ] = [...section2.split("-")].map( ele => parseInt(ele))
-	if ((section1First >= section2First) && (section1Second <= section2Second)){
-		counter++;	
-	}
-	else if ((section2First >= section1First) && (section2Second <= section1Second)){
+	const set1 = new Set(getRange(((section1Second - section1First) + 1), section1First));
+	const set2 = new Set(getRange(((section2Second - section2First) + 1), section2First));
+	const intersection = new Set([...set1].filter( x => set2.has(x)));	
+	if (eqSetCheck(set1, intersection) || eqSetCheck(set2, intersection)) {
 		counter++;
 	}
+
 })
 
-console.log(counter);
+console.log({ counter });
--- advent of code/day4/part2.js
+++ advent of code/day4/part2.js
@@ -3,22 +3,21 @@ const { data } = require('./input')
 
 counter = 0;
 
+const getRange = (size, startAt = 0) => {
+	return [...Array(size).keys()].map( i => i + startAt);
+}
+
 data.forEach((ele) => {
 	const [ section1, section2 ] = [...ele.split(",")]
 	const [ section1First, section1Second ] = [...section1.split("-")].map( ele => parseInt(ele))
 	const [ section2First, section2Second ] = [...section2.split("-")].map( ele => parseInt(ele))
-	if ((section1First >= section2First) && (section1First <= section2Second)){
+	const set1 = new Set(getRange(((section1Second + 1) - section1First), section1First));
+	const set2 = new Set(getRange(((section2Second + 1) - section2First), section2First));
+	const set3 = [...set1].filter(x => set2.has(x));
+	if (set3.length > 0) {
 		counter++;
 	}
-	else if((section1Second >= section2First) && (section1Second <= section2Second)) {
-		counter++;
-	}
-	else if((section2First >= section1First) && (section2First <= section1Second)) {
-		counter++;
-	}
-	else if((section2Second >= section1First) && (section2Second <= section1Second)) {
-		counter++;
-	}
+	
 })
 
 console.log(counter);
